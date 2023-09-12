93860a0721ae618ad8633e2c585bc2823976ceb0
See <meer3200@gmail.com>
2023-09-12 16:22:12 +0300

    initial commit

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import classes from "./Counter.module.scss"
+
+const AboutPage = () => {
+
+  return <div>
+About page
+  </div>
+};
+
+export default AboutPage;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import classes from "./Counter.module.scss"
+
+const MainPage = () => {
+
+  return <div>
+Main Page
+  </div>
+};
+
+export default MainPage;
